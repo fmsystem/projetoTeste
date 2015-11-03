8667806ca107a554e101d144efb0166bf577063e
fmsystem <fabio.func@liteci.com.br>
2015-11-03 14:14:14 -0300

    inicio

---
+++ Project.dpr
@@ -0,0 +1,14 @@
+program Project;
+
+uses
+  Vcl.Forms,
+  tela in 'tela.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
