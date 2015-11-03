2e474448d4137b0c2c44303f48c46469cc42c51b
fmsystem <fabio.func@liteci.com.br>
2015-11-02 21:16:20 -0300

    painel

--- tela.pas
+++ tela.pas
@@ -4,13 +4,14 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
 
 type
   TForm2 = class(TForm)
     Button1: TButton;
     Button2: TButton;
     Button3: TButton;
+    Panel1: TPanel;
   private
     { Private declarations }
   public
