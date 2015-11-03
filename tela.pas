c8d27df715d67717c7e3e9de4333c61f13f2c747
fmsystem <fabio.func@liteci.com.br>
2015-11-02 21:39:25 -0300

    proc

---
+++ tela.pas
@@ -0,0 +1,38 @@
+unit tela;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm2 = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    procedure TesteFabio;
+    procedure TesteOutro;
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+{ TForm2 }
+
+procedure TForm2.TesteFabio;
+begin
+
+end;
+
+procedure TForm2.TesteOutro;
+begin
+
+end;
+
+end.
