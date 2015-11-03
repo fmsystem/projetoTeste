20fdeedc749195a923c2266868b5648def284769
YallisonFelipe <felipe@liteci.com.br>
2015-11-03 14:00:20 -0300

    Real

--- tela.dfm
+++ tela.dfm
@@ -33,6 +33,7 @@ object Form2: TForm2
     Height = 25
     Caption = 'Button1'
     TabOrder = 0
+    OnClick = Button1Click
   end
   object Memo1: TMemo
     Left = 8
--- tela.pas
+++ tela.pas
@@ -3,7 +3,8 @@ unit tela;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Buttons, Vcl.StdCtrls;
 
 type
@@ -11,20 +12,27 @@ type
     SpeedButton1: TSpeedButton;
     Label1: TLabel;
     Memo1: TMemo;
-
-type
-  TForm2 = class(TForm)
     Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
-    { Public declarations }
+    Procedure PeloSportTudo;
   end;
-
-var
 
 implementation
 
 {$R *.dfm}
+{ TForm2 }
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  PeloSportTudo;
+end;
+
+procedure TForm2.PeloSportTudo;
+begin
+  ShowMessage('Cazá,Cazá');
+end;
 
 end.
