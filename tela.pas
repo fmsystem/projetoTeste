e04cc412fd02ad8dc8b734e0123176a23469d213
fmsystem <fabio.func@liteci.com.br>
2015-11-03 14:18:17 -0300

    tela

--- tela.pas
+++ tela.pas
@@ -4,10 +4,12 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
 
 type
   TForm2 = class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
   private
     { Private declarations }
   public
